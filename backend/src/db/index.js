4cc0c189521bb1e7c264d3bad790aa913a3ef4e3
sidhant singh <162982475+sid446@users.noreply.github.com>
2024-10-30 23:17:39 +0530

    Update index.js

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constant.js";
 
 const connectDB=async ()=>{
     try {
-        const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const connectionInstance= await mongoose.connect(`mongodb+srv://sidhantsingh:sidbrogo123@sidhant.putrf.mongodb.net/${DB_NAME}`);
         console.log(`\nMongoDB cennected !! DB host: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.error("MONGODB connection Failed",error);
