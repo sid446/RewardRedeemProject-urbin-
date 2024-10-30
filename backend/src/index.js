9e75d1badba4fee7a6639a869e67d4e7ea66f19a
sidhant singh <162982475+sid446@users.noreply.github.com>
2024-10-30 23:21:51 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ dotenv.config({
  
 connectDB()
 .then(()=>{
-    app.listen(8000,()=>{
+    app.listen(process.env.PORT || 8000,()=>{
         console.log(`Server is running at Port :8000`);
     })
 })
